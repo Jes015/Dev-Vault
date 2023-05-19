b4308e2c13268e56c3e801d9e7c07c4530865629
Jes015 <jes.18.sin.15@gmail.com>
2023-05-19 20:59:50 +0000

    feat: update ISSG time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,6 @@ import helpHttps from '@/helpers/helpHttp'
 // Types
 import type { TTools } from '@/types/apptypes'
 
-// Mocks
-
 interface props {
   dynamicTools: TTools
 }
@@ -29,12 +27,13 @@ export default App
 
 export async function getStaticProps () {
   const apikey = process.env.JSONBIN as string
+  console.log(apikey)
   const dynamicTools = (await helpHttps().get('https://api.jsonbin.io/v3/b/6444893eb89b1e22998fa0ee', apikey)).record
 
   return {
     props: {
       dynamicTools
     },
-    revalidate: 30
+    revalidate: 21600 // 6 hours
   }
 }
